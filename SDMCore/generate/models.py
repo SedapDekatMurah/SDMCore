020d409d3ead77dbe98f75e01245b0a5ca244003
ChickyMasala <61800906+SammyMasala@users.noreply.github.com>
2024-03-12 08:23:55 +0800

    Update models.py

--- SDMCore/generate/models.py
+++ SDMCore/generate/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+# import generation_scripts
 
 # Create your models here.
+# def generate_data
+# # reply_message = ""
+
+# # try 
+# # # conn.cursor = open (database)
+# # # catch if database failed:
+# # # # throw err
+
+# # # restaurants_data = #generate_script_function().json()
+# # # catch if data generation failed:
+# # # # throw err
+
+# # # for restaurant in restaurants_data:
+# # # # conn.execute('''INSERT INTO TABLE restaurants WHERE NOT EXISTS (?,?,?,?,?) (...values)'''
+# # # # for review in restaurant['reviews']
+# # # # # conn.execute('''INSERT INTO TABLE reviews WHERE NOT EXISTS (?,?,?,?,?) (...values)'''
+
+# # # close when succesful
+# # # conn.commit()
+# # # conn.close()
+
+# # # return message
+# # # reply_message = "Restaurant data generated for {latitude}, {longitude}
+
+# # catch
+# # # reply_message = "Encountered server error"
+
+# # finally
+# # # return reply_message
