c0ead7fda546aec0d48794da59ccd24eee4bd803
ChickyMasala <61800906+SammyMasala@users.noreply.github.com>
2024-03-12 08:31:24 +0800

    Update tests.py

--- SDMCore/generate/tests.py
+++ SDMCore/generate/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+# model.py
+# # generate_data function
+# # should take 2 variables: latitude, longitude
+# # should return error message on a failure
+# # should return success message on a pass
+
+# views.py
+# # post_generate function
+# # should take 1 variable: packet
+# # should 
