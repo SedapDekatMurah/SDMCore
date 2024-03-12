83be8501b6b0736a29a333036c0be2a12ae19a10
ChickyMasala <61800906+SammyMasala@users.noreply.github.com>
2024-03-12 08:09:40 +0800

    Update views.py

--- SDMCore/generate/views.py
+++ SDMCore/generate/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def view(post-generate)
+# # data = views.generate_data
+# # return ("data generated for {latitude}, {longitude} )
