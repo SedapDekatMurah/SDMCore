961527abe3694bc167d5447dd61d92c2097b0542
SammyMasala <samuel.goh96@gmail.com>
2024-03-17 18:20:28 +0800

    started testing

--- SDMCore/tests/tests.py
+++ SDMCore/tests/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from unittest.mock import patch
-from SDMCore.core.scripts import save, places, keywords, menu, generate
-from SDMCore.core.scripts import generate
+from SDMCore.core.scripts import generate, places
 
 # generate-scripts/
 
@@ -10,13 +9,14 @@ from SDMCore.core.scripts import generate
 # generate_data function
 
 
-class GenerateDataTestCase(TestCase):
+class TestGenerateData(TestCase):
+    def mock_function():
+        return {'ErrorStatus': 'test'}
     # get_restaurant returns 'ErrorStatus'
-    @patch('places.get_restaurant')
-    def get_restaurants_error_returns_false(self, mock_get_restaurant):
 
-        # Mock return value of places.get_restaurants
-        mock_get_restaurant.return_value = {'ErrorStatus': 'test'}
+    # @patch('SDMCore.core.scripts.places', get_restaurant=mock_function)
+    def test_get_restaurants_error(self, mock_get_restaurant):
+        print(places.get_restaurants("10", "10"))
 
         # Assert that getting 'ErrorStatus' from places.get_restaurants will cause generate_data to return False
         self.assertEqual(generate.generate_data("10", "10"), False)
