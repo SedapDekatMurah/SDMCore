aaeaae7106d0a450939ac0eeee633fbd7164bab4
SammyMasala <samuel.goh96@gmail.com>
2024-03-17 12:22:08 +0800

    update menu.py

--- SDMCore/generate/scripts/menu.py
+++ SDMCore/generate/scripts/menu.py
@@ -1,3 +1,20 @@
 def generate_menu():
     # TODO
+
+    # Pipeline 1
+    # get a list of recent photos tagged 'menu'
+
+    # OCR scan for list of food and prices
+
+    # scrape reviews for highly mentioned food items
+
+    # Fill profile with top 3
+
+    # Pipeline 2
+    # scrape recent reviews for food and prices
+
+    # Fill profile with top 3
+
+    #
+
     return True
